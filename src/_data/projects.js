3bfc307f011acf973b6f7028188d040be27b059f
elliot.moore <elliotmoorecornwall@outlook.com>
2024-04-10 14:58:00 +0100

    Fixed typo in projects.js `consultations`

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,5 +1,5 @@
 module.exports = [
-  {title: "Consulations", url: "/projects/consultations", img: "/img/consultation4.png", synopsis: "An AI-powered tool to automate the processing of public consultations"},
+  {title: "Consultations", url: "/projects/consultations", img: "/img/consultation4.png", synopsis: "An AI-powered tool to automate the processing of public consultations"},
   {title: "Redbox Copilot", url: "/projects/redbox-copilot", img: "/img/redbox3.png", synopsis: "Bringing Generative AI to the way the Civil Service works"},
   {title: "Caddy", url: "/projects/caddy", img: "/img/caddy1.png", synopsis: "Our AI powered copilot for customer service agents across government and beyond"},
   {title: "rAPId", url: "/projects/rapid", img: "/img/rapid2.webp", synopsis: "An end-to-end solution to sharing data across government"}
