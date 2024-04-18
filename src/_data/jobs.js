ab64f74ea859901ce8a4122b0d3da618a98f730b
Kevin Etchells <kevetchells@hotmail.com>
2024-04-18 15:41:48 +0100

    Update active jobs

--- src/_data/jobs.js
+++ src/_data/jobs.js
@@ -1,37 +1,31 @@
 module.exports = [
-  /*
   {
-    title: "Head of Impact and Evaluation",
+    title: "AI Head of Impact and Evaluation",
     grade: "Grade 6",
-    salary: "£64,700 - £98,975",
+    salary: "£64,700 - £79,073",
     location: "Bristol, Glasgow, London, Manchester, York",
     headline: "",
     description: {
       about: [
-        `The Incubator for Artificial Intelligence (i.AI) leads the Cabinet Office AI for Public Good programme to harness the opportunities presented by AI to improve the lives of citizens. It focuses on a number of priority projects in home affairs, health, education and government efficiency projects. The i.AI team uses ethical and secure methods at all times to help deliver better public services.`,
+        `The Incubator for Artificial Intelligence (i.AI) - is an AI for public good programme to harness the opportunities presented by AI to improve the lives of citizens. It focuses on a number of priority projects in home affairs, health, education and government efficiency projects. The i.AI team uses ethical and secure methods at all times to help deliver better public services.`,
         `Building on the work of No10 Data Science Team (10DS) and other leading technology teams across the civil service, i.AI will experiment and prove what is possible in improving the use of AI across the government. It was recently announced that i.AI will expand a further 40 roles to build a scale up function, which will take successful incubated projects into production.`,
         `i.AI’s <strong>Impact and Evaluation</strong> team will build their work into each stage of product development, from scoping, to incubation, and onwards to scaling. Team members will embed into product development teams, designing and delivering of randomised control trials, impact evaluations, and qualitative studies primarily into the efficiency and quality impacts of our AI products.`
       ],
       responsibilities: [
-        `Setting up and leading i.AI’s Impact and Evaluation function.`,
-        `Leading the design and implementation of evaluations for our AI products, including developing evaluation frameworks, identifying appropriate methodologies, and collecting and analysing data.`,
-        `Communicating the results of your team’s evaluations in a way that is engaging and supports delivery of the mission of i.AI managing and mentoring Impact and Evaluation Leads (3FTE), providing guidance and support to ensure high-quality work.`,
         `Embedding impact and evaluation into AI products from the very start and throughout the development cycle.`,
-        `Leading the team to deliver wider AI evaluation throughout the development cycle. Experience in this is not required, as these are new and emerging methods that you would be key in forging practical delivery of: safety assessment, algorithmic transparency reporting, red teaming to assess AI bias and safety.`,
-        `Building evidence led and data driven cases for i.AI funding bids in processes such as Budgets or Spending Reviews.`,
-        `Developing data collection tools, such as surveys, interviews, and focus groups, and ensuring data quality.`,
-        `Conducting data analysis, drawing insights and recommendations and presenting them stakeholders.`
+        `We will teach you to apply your skills to deliver wider AI evaluation throughout the development cycle. These are new and emerging methods that you would be key in forging practical delivery of: safety assessment, algorithmic transparency reporting, red teaming to assess AI bias and safety.`,
+        `Building evidence-led and data driven cases for i.AI funding bids in processes such as Budgets or Spending Reviews.`,
+        `Conducting data analysis, drawing insights and recommendations and presenting them to stakeholders.`
       ],
       skills: [
-        `Excellent analytical and critical thinking skills, with the ability to synthesise complex information and draw insights from data.`,
-        `Experience of leading, overseeing and motivating researchers to deliver high impact research and of managing its technical direction.`,
-        `Experience of leading on cost-benefit analysis and policy appraisal in line with the Green Book.`,
         `Experience of designing, leading and delivering research, from a relevant discipline such as an economist, behavioural scientist, or a social scientist.`,
+        `Excellent analytical and critical thinking skills, with the ability to synthesise complex information and draw insights from data.`,
         `Ability to engage and influence others at various levels of seniority to maximise the impact of research studies.`,
-        `Considerable knowledge of qualitative and quantitative research design, methods and analysis.`,
+        `Knowledge of either quantitative and/or qualitative research design, methods and analysis.`,
         `Ability to work collaboratively and across disciplines, and maintain strong working relationships with a broad range of individuals.`,
         `Ability to communicate research for varied audiences, using different media/formats e.g. technical reports; presentations; web-based tools.`,
-        `You need to be, or will be willing to become badged in one of the following professions: economist, social researcher, statistician, operational researcher, or be part of the DDAT profession.`,
+        `Non-essential but beneficial: experience of leading on cost-benefit analysis and policy appraisal in line with the Green Book`,
+        `Needs to be/become DDAT/badged Economist/Social Researcher/Statistician/Operational Researcher.`,
         `In-depth expertise of a number of commonly used evaluation methods.`,
         `Experimental and quasi-experimental methods are particularly desirable.`,
         `Excellent communication skills, both verbal and written, with the ability to communicate technical information on evaluations to non-specialists in ways that are easily understood.`,
@@ -39,12 +33,12 @@ module.exports = [
         `Coding ability in python or similar languages would be beneficial.`
       ]
     },
-    link: "https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID=cGFnZWFjdGlvbj12aWV3dmFjYnlqb2JsaXN0JmpvYmxpc3Rfdmlld192YWM9MTkwNjMzOCZvd25lcnR5cGU9ZmFpciZzZWFyY2hzb3J0PXNjb3JlJnVzZXJzZWFyY2hjb250ZXh0PTc2ODc5MjQzJnNlYXJjaHBhZ2U9MSZvd25lcj01MDcwMDAwJnBhZ2VjbGFzcz1Kb2JzJnJlcXNpZz0xNzEzMTg0MzU3LTg1ODhlZTg5NWNmMDgyOTczZDdkNWRjMGRjMTc1ZWRjOWY3OWQ0NzI="
+    link: "https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID=b3duZXI9NTA3MDAwMCZwYWdlYWN0aW9uPXZpZXd2YWNieWpvYmxpc3Qmc2VhcmNocGFnZT0xJm93bmVydHlwZT1mYWlyJnBhZ2VjbGFzcz1Kb2JzJmpvYmxpc3Rfdmlld192YWM9MTkwNjMzOCZ1c2Vyc2VhcmNoY29udGV4dD03NzM2MjA5OSZzZWFyY2hzb3J0PXNjb3JlJnJlcXNpZz0xNzEzNDQ4NDk3LWYwZjdmMjI3N2JiNjc0YThjOTE0Y2RkNzJmMzZlZWI4NzIwOTQ5ZDU="
   },
   {
     title: "AI Impact and Evaluation Lead",
     grade: "Grade 7",
-    salary: "£53,400 - £85,600",
+    salary: "£53,400 - £66,875",
     location: "Bristol, Glasgow, London, Manchester, York",
     headline: "",
     description: {
@@ -75,7 +69,6 @@ module.exports = [
         `Coding ability in python or similar languages are not essential but would be beneficial.`
       ]
     },
-    link: "https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID=b3duZXI9NTA3MDAwMCZzZWFyY2hwYWdlPTEmcGFnZWNsYXNzPUpvYnMmdXNlcnNlYXJjaGNvbnRleHQ9NzcwMDg3NTUmcGFnZWFjdGlvbj12aWV3dmFjYnlqb2JsaXN0JnNlYXJjaHNvcnQ9c2NvcmUmam9ibGlzdF92aWV3X3ZhYz0xOTA2NDM4Jm93bmVydHlwZT1mYWlyJnJlcXNpZz0xNzEzMjU4NDg2LTIyZmNkY2UxZjU1NTU0OTczNThkOGY0MTg2YjJhMWU0MTdkZTM3MzA="
+    link: "https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID=c2VhcmNocGFnZT0xJm93bmVydHlwZT1mYWlyJnBhZ2VjbGFzcz1Kb2JzJmpvYmxpc3Rfdmlld192YWM9MTkwNjQzOCZvd25lcj01MDcwMDAwJnBhZ2VhY3Rpb249dmlld3ZhY2J5am9ibGlzdCZzZWFyY2hzb3J0PXNjb3JlJnVzZXJzZWFyY2hjb250ZXh0PTc3MzYyMDk5JnJlcXNpZz0xNzEzNDQ4NDk3LWYwZjdmMjI3N2JiNjc0YThjOTE0Y2RkNzJmMzZlZWI4NzIwOTQ5ZDU="
   }
-  */
 ];
