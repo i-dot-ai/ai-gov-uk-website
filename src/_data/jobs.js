95c0e9290bd99f6432c17c7e5f6387c25705dba7
Kevin Etchells <kevetchells@hotmail.com>
2024-04-26 09:44:53 +0100

    Update job role

--- src/_data/jobs.js
+++ src/_data/jobs.js
@@ -12,20 +12,25 @@ module.exports = [
         `i.AI’s <strong>Impact and Evaluation</strong> team will build their work into each stage of product development, from scoping, to incubation, and onwards to scaling. Team members will embed into product development teams, designing and delivering of randomised control trials, impact evaluations, and qualitative studies primarily into the efficiency and quality impacts of our AI products.`
       ],
       responsibilities: [
+        `Setting up and leading i.AI’s Impact and Evaluation function.`,
+        `Leading the design and implementation of evaluations for our AI products, including developing evaluation frameworks, identifying appropriate methodologies, and collecting and analysing data.`,
+        `Communicating the results of your team’s evaluations in a way that is engaging and supports delivery of the mission of i.AI managing and mentoring Impact and Evaluation Leads (3FTE), providing guidance and support to ensure high-quality work.`,
         `Embedding impact and evaluation into AI products from the very start and throughout the development cycle.`,
-        `We will teach you to apply your skills to deliver wider AI evaluation throughout the development cycle. These are new and emerging methods that you would be key in forging practical delivery of: safety assessment, algorithmic transparency reporting, red teaming to assess AI bias and safety.`,
-        `Building evidence-led and data driven cases for i.AI funding bids in processes such as Budgets or Spending Reviews.`,
-        `Conducting data analysis, drawing insights and recommendations and presenting them to stakeholders.`
+        `Leading the team to deliver wider AI evaluation throughout the development cycle. Experience in this is not required, as these are new and emerging methods that you would be key in forging practical delivery of: safety assessment, algorithmic transparency reporting, red teaming to assess AI bias and safety.`,
+        `Building evidence led and data driven cases for i.AI funding bids in processes such as Budgets or Spending Reviews.`,
+        `Developing data collection tools, such as surveys, interviews, and focus groups, and ensuring data quality.`,
+        `Conducting data analysis, drawing insights and recommendations and presenting them stakeholders.`
       ],
       skills: [
-        `Experience of designing, leading and delivering research, from a relevant discipline such as an economist, behavioural scientist, or a social scientist.`,
         `Excellent analytical and critical thinking skills, with the ability to synthesise complex information and draw insights from data.`,
+        `Experience of leading, overseeing and motivating researchers to deliver high impact research and of managing its technical direction.`,
+        `Experience of leading on cost-benefit analysis and policy appraisal in line with the Green Book.`,
+        `Experience of designing, leading and delivering research, from a relevant discipline such as an economist, behavioural scientist, or a social scientist.`,
         `Ability to engage and influence others at various levels of seniority to maximise the impact of research studies.`,
-        `Knowledge of either quantitative and/or qualitative research design, methods and analysis.`,
+        `Considerable knowledge of qualitative and quantitative research design, methods and analysis.`,
         `Ability to work collaboratively and across disciplines, and maintain strong working relationships with a broad range of individuals.`,
         `Ability to communicate research for varied audiences, using different media/formats e.g. technical reports; presentations; web-based tools.`,
-        `Non-essential but beneficial: experience of leading on cost-benefit analysis and policy appraisal in line with the Green Book`,
-        `Needs to be/become DDAT/badged Economist/Social Researcher/Statistician/Operational Researcher.`,
+        `You need to be, or will be willing to become badged in one of the following professions: economist, social researcher, statistician, operational researcher, or be part of the DDAT profession.`,
         `In-depth expertise of a number of commonly used evaluation methods.`,
         `Experimental and quasi-experimental methods are particularly desirable.`,
         `Excellent communication skills, both verbal and written, with the ability to communicate technical information on evaluations to non-specialists in ways that are easily understood.`,
