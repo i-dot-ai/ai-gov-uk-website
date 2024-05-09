e5e083bdd938d7ea90660552d7aececf5fae1da1
Kevin Etchells <kevetchells@hotmail.com>
2024-05-09 12:30:19 +0100

    Get pages from sitemap for accessibility tests

--- tests/accessibility.spec.ts
+++ tests/accessibility.spec.ts
@@ -2,27 +2,28 @@ import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
 const urlPrefix = 'http://localhost:8080/';
-const pagesToTest = [
-  '',
-  'about',
-  'join',
-  'projects',
-  'projects/consultations',
-  'projects/redbox-copilot',
-  'projects/caddy',
-  'projects/rapid',
-  'projects/nhs-collaboration',
-  'accessibility',
-  'site-map'
-];
 
-for (let i = 0; i < pagesToTest.length; i++) {
 
-  test(`Accessibility: ${pagesToTest[i]}`, async ({ page }) => {
+test(`Accessibility`, async ({ page }) => {
 
-    await page.goto(`${urlPrefix}${pagesToTest[i]}`);
+  // Collect all pages from sitemap
+  let pagesToTest = [''];
+  await page.goto(`${urlPrefix}site-map`);
+  const links = await page.$$('a');
+  for (let link of links) {
+    const url = (await link.getAttribute('href'))?.replace('/', '');
+    if (url && !url.includes('mailto') && !url.startsWith('#') && !pagesToTest.includes(url)) {
+      pagesToTest.push(url);
+    }
+  }
+
+  // Run accessibility tests
+  for (let pageToTest of pagesToTest) {
+
+    await page.goto(`${urlPrefix}${pageToTest}`);
     await page.waitForTimeout(1000); // allow animation to run (otherwise may get a false positive for colour contrast)
-
+    console.log(`Testing accessibility for ${pageToTest}`);
+    
     // Axe tests
     const accessibilityScanResults = await new AxeBuilder({page}).analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
@@ -37,6 +38,6 @@ for (let i = 0; i < pagesToTest.length; i++) {
       }
     }
 
-  });
+  }
 
-}
+});
