de7ac42da3c215e7b09254defdf51c0999a174e6
Kevin Etchells <kevetchells@hotmail.com>
2024-06-11 13:29:27 +0100

    Pull in jobs data from CMS and remove full jobs pages

--- src/_data/jobs.js
+++ src/_data/jobs.js
@@ -1,35 +1,31 @@
-module.exports = [
-  /*
-  {
-    title: "Deputy Director, i.AI Strategy and Business Engagement",
-    grade: "SCS 1",
-    salary: "£75,000 - £117,800",
-    location: "London",
-    headline: "We are recruiting for Deputy Director i.AI Strategy and Business Engagement, to support i.AI in their mission to identify and deploy AI for the public good. This role will support that mission by working across government, and representing the Cabinet Office externally, to ensure that i.AI is effectively joined up with the thriving UK AI ecosystem.",
-    description: {
-      about: [
-        `This is an exciting opportunity to join a brand new team operating at the heart of government. The incubator for Artificial Intelligence (i.AI) is a brilliant new team, comprised of technical experts who are set up to harness the opportunity of AI to improve lives, generate efficiency savings and deliver better public services.`,
-        `This role is a critical one, sitting at the heart of our structure and overseeing the key enabling functions for our team - Strategy, Operations and Impact. You will work closely with technical experts from across the Digital Data and Technology (DDaT) profession, including AI Engineers, Data Scientists, Software Developers, User Researchers and more. These technical roles form part of our product teams, who are building tools for i.AI to deploy across government.`,
-        `This role will have line management responsibility for three teams, covering <strong>strategy, business operations, and impact and evaluation</strong>. Taken together, these functions will provide support to our product team and make sure that we are able to interface effectively with the rest of government in delivering our mission.`
-      ],
-      responsibilities: [
-        `You will constructively engage across government, building effective working relationships, and engaging with senior stakeholders to champion AI and data driven solutions.`,
-        `You will demonstrate a strong ability to form effective collaborations and manage very senior stakeholders, while leading by example for analysts, technical staff, and policy teams across government to inspire a step change in innovation practice use and technical upskilling.`,
-        `You will be credible externally in discussing AI for the public good and setting a vision for the UK’s adoption of this exciting technology, including effective engagement with Government AI ministers, and meeting regularly with business leaders and representing the UK government position.`,
-        `You will manage three critical branches in the team covering strategy, business operations, and impact and evaluation.`,
-        `You will report to Dr Laura Gilbert (10 Downing Street), Director of i.AI and Senior Responsible Officer (SRO) of AI for the Public Good and work very closely with ministerial offices to ensure strategic alignment.`
-      ],
-      skills: [
-        `Proven experience of operating effectively in a relevant tech-adjacent field, for example experience in both AI strategy and implementation, wider digital/analysis functions in government or a large, complex tech-focussed organisation.`,
-        `Evidenced ability to build and maintain relationships with, and secure the confidence and trust of stakeholders, senior leaders, and peers, through the provision of expert advice on strategy, partnership working, and collaborative project delivery.`,
-        `Ability to work at pace, handling competing priorities and challenging deadlines in complex stakeholder environments. Rapid, collaborative problem solving and resolving complex issues when they arise.`,
-        `Excellent communication and written skills, with a proven track record of influencing effectively at Ministerial and Permanent Secretary level; or Board Level.`,
-        `Experience as a confident and empowering leader who sets a clear strategic direction to deliver key priorities through teams, with a track record of motivating, developing and successfully leading diverse, high-performing teams.`,
-        `Experience of setting up new teams, establishing good ways of working and effective partnership models.`,
-        `Experience of working with external business partners/collaborators to drive change within large, complex organisations and with stakeholders relevant to the mission of i.AI.`
-      ]
-    },
-    link: "https://www.civilservicejobs.service.gov.uk/csr/jobs.cgi?jcode=1908736"
-  },
-  */
-];
+require('dotenv').config();
+const contentful = require('contentful');
+
+const client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+});
+  
+module.exports = () => {
+
+    return client.getEntries({
+        content_type: 'jobs'
+    }).then((response) => {
+
+        let jobs = response.items.map((role) => {
+            return role.fields;
+        });
+
+        // sort by title
+        jobs.sort((a, b) => {
+            return a.title < b.title ? -1 : 1;
+        });
+
+        return jobs;
+
+    }).catch((err) => {
+        console.error(err);
+    });
+
+};
+  
