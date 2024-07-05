50d8ba0fcf2cd7ff33445038a80d678133bf41a2
Kevin Etchells <kevetchells@hotmail.com>
2024-07-05 15:57:44 +0100

    Replace NHS image

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -3,5 +3,5 @@ module.exports = [
   {title: "Redbox", url: "/projects/redbox", img: "/img/redbox4.png", synopsis: "Redbox is a service that harnesses AI to help you summarise, and ask questions of, documents up to OFFICIAL-SENSITIVE"},
   {title: "Caddy", url: "/projects/caddy", img: "/img/caddy1.png", synopsis: "Our AI powered copilot for customer service agents across government and beyond"},
   {title: "rAPId", url: "/projects/rapid", img: "/img/rapid2.webp", synopsis: "An end-to-end solution to sharing data across government"},
-  {title: "i.AI and NHS England Collaboration Charter", url: "/projects/nhs-collaboration", img: "/img/nhs1.jpg", synopsis: "i.AI and NHS England sign Collaboration Charter to support the use of AI in the NHS"}
+  {title: "i.AI and NHS England Collaboration Charter", url: "/projects/nhs-collaboration", img: "/img/nhs2.png", synopsis: "i.AI and NHS England sign Collaboration Charter to support the use of AI in the NHS"}
 ].reverse();
