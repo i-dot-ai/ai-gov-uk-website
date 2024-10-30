907ca30284c125de97b3383827fb6ee3935ed815
Kevin Etchells <kevetchells@hotmail.com>
2024-10-30 11:08:52 +0000

    Add previews for Project pages on CMS

--- src/_data/projects-new.js
+++ src/_data/projects-new.js
@@ -1,7 +1,37 @@
+/*
+    This exists because we don't want to create pages for all projects (i.e. the earlier ones that have been created manually)
+    This is also a good place to add the preview page
+*/
+
 const getProjects = require("./projects.js");
 
 module.exports = async () => {
-    const allProjects = await getProjects();
+    let allProjects = await getProjects();
+
+    allProjects.push({
+        title: "Preview",
+        synopsisHeader: ["Summary to go here"],
+        components: [
+            {
+                type: "doubleColumn",
+                order: "Text (left) - Image (right)",
+                content: "[Content to go here]",
+                image: ""
+            },
+            {
+                type: "doubleColumn",
+                order: "Image (left) - Text (right)",
+                content: "[Content to go here]",
+                image: ""
+            },
+            {
+                type: "video",
+                source: "",
+                audioDescription: ""
+            },
+        ]
+    });
+
     return allProjects.filter((project) => {
         return !project.url;
     });
