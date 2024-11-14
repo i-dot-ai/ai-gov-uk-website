b99b405c38c8585421f2406fdb0f6baa89d98d17
Kevin Etchells <kevetchells@hotmail.com>
2024-11-14 11:08:15 +0000

    Serve images from CMS

--- src/_data/blogs.js
+++ src/_data/blogs.js
@@ -1,159 +1,158 @@
-require('dotenv').config();
-const contentful = require('contentful');
-const matter = require('gray-matter');
+require("dotenv").config();
+const contentful = require("contentful");
+const matter = require("gray-matter");
 const getData = require("./_shared.js").getData;
-const downloadImage = require("./_shared.js").downloadImage;
 
 const USE_PREVIEW = false;
 const CMS_REPO = "i-dot-ai/ai-gov-uk-cms-content";
 
 let contentfulOptions = {
-    space: process.env.CONTENTFUL_SPACE,
-    accessToken: USE_PREVIEW ? process.env.PREVIEW_ACCESS_TOKEN : process.env.CONTENTFUL_ACCESS_TOKEN
+  space: process.env.CONTENTFUL_SPACE,
+  accessToken: USE_PREVIEW
+    ? process.env.PREVIEW_ACCESS_TOKEN
+    : process.env.CONTENTFUL_ACCESS_TOKEN,
 };
 if (USE_PREVIEW) {
-    contentfulOptions.host = 'preview.contentful.com';
+  contentfulOptions.host = "preview.contentful.com";
 }
 const client = contentful.createClient(contentfulOptions);
 
-
 module.exports = async () => {
+  let response = await client.getEntries({
+    content_type: "blog",
+  });
+  let blogs = response.items.map((blog) => {
+    return {
+      title: blog.fields.title,
+      summaryShort: blog.fields.summary,
+      summaryLong: blog.fields.summaryBlogPage,
+      authors: blog.fields.authors.map((author) => author.fields),
+      date: blog.fields.date,
+      coverImage: blog.fields.coverImage?.fields,
+      content: blog.fields.content,
+      contentPart2: blog.fields.contentPart2,
+      source: "Contentful",
+    };
+  });
 
-    let response = await client.getEntries({
-        content_type: 'blog'
+  // Get new authors
+  const newAuthors = await getData(
+    `https://api.github.com/repos/${CMS_REPO}/contents/content/authors`
+  );
+  let allAuthors = [];
+  for (const author of newAuthors.map((item) => item.url)) {
+    const authorRawData = await getData(author);
+    const authorContent = Buffer.from(authorRawData.content, "base64").toString(
+      "utf8"
+    );
+    const authorData = matter(authorContent).data;
+
+    allAuthors.push({
+      name: authorData.name,
+      jobTitle: authorData.jobTitle,
+      picture: {
+        fields: {
+          file: {
+            url: authorData.picture,
+          },
+        },
+      },
     });
-    let blogs = response.items.map((blog) => {
-        return {
-            title: blog.fields.title,
-            summaryShort: blog.fields.summary,
-            summaryLong: blog.fields.summaryBlogPage,
-            authors: blog.fields.authors.map((author) => author.fields),
-            date: blog.fields.date,
-            coverImage: blog.fields.coverImage?.fields,
-            content: blog.fields.content,
-            contentPart2: blog.fields.contentPart2,
-            source: "Contentful"
-        };
-    });
+  }
 
+  // Get new blogs
+  const newBlogs = await getData(
+    `https://api.github.com/repos/${CMS_REPO}/contents/content/blogs`
+  );
+  for (const blog of newBlogs.map((item) => item.url)) {
+    const blogRawData = await getData(blog);
+    const blogContent = Buffer.from(blogRawData.content, "base64").toString(
+      "utf8"
+    );
+    const blogData = matter(blogContent).data;
 
-    // Get new authors
-    const newAuthors = await getData(`https://api.github.com/repos/${CMS_REPO}/contents/content/authors`);
-    let allAuthors = [];
-    for (const author of newAuthors.map(item => item.url)) {
-        const authorRawData = await getData(author);
-        const authorContent = Buffer.from(authorRawData.content, "base64").toString("utf8");
-        const authorData = matter(authorContent).data;
-        const picture = authorData.picture.replace("/images/uploads/", "");
-        await downloadImage(picture);
-        allAuthors.push({
-            name: authorData.name,
-            jobTitle: authorData.jobTitle,
-            picture: {
-                fields: {
-                    file: {
-                        url: `/img/from-cms/${picture}`
-                    }
-                }
-            }
-        });
+    const image = (() => {
+      if (blogData.leadImage.includes("http:")) {
+        return blogData.leadImage.replace("http:", "https:");
+      }
+      const returnUrl = blogData.leadImage;
+      return returnUrl;
+    })();
+
+    // get all images within the content
+    for (let component of blogData.components || []) {
+      if (component.type === "bodyText") {
+        const imagePaths = component.content.match(
+          /\/images\/uploads\/[a-z0-9\.]*/g
+        );
+      }
     }
 
-
-    // Get new blogs
-    const newBlogs = await getData(`https://api.github.com/repos/${CMS_REPO}/contents/content/blogs`);
-    for (const blog of newBlogs.map(item => item.url)) {
-        const blogRawData = await getData(blog);
-        const blogContent = Buffer.from(blogRawData.content, "base64").toString("utf8");
-        const blogData = matter(blogContent).data;
-
-        const image = (() => {
-            if (blogData.leadImage.includes("http:")) {
-                return blogData.leadImage.replace("http:", "https:");
-            }
-            const returnUrl = blogData.leadImage.replace("/images/uploads/", "");
-            downloadImage(returnUrl);
-            return returnUrl;
-        })();
-
-        // get all images within the content
-        for (let component of blogData.components || []) {
-            if (component.type === "bodyText") {
-                const imagePaths = component.content.match(/\/images\/uploads\/[a-z0-9\.]*/g);
-                if (imagePaths) {
-                    for (const imagePath of imagePaths) {
-                        await downloadImage(imagePath.replace("/images/uploads/", ""));
-                    }
-                }
-                component.content = component.content.replace(/images\/uploads/g, "img");
-            }
-        }
-
-        blogs.push({
-            title: blogData.title,
-            summaryShort: blogData.summaryHubPage,
-            summaryLong: blogData.summaryBlogPage,
-            authors: (() => {
-                let blogAuthors = blogData.authors?.map((author) => {
-                    return author.author;
-                });
-                let foundAuthors = [];
-                allAuthors.forEach((author) => {
-                    if (blogAuthors?.includes(author.name)) {
-                        foundAuthors.push(author);
-                    }
-                });
-                return foundAuthors;
-            })(),
-            date: blogData.date,
-            coverImage: {
-                title: blogData.leadImageCaption,
-                description: blogData.leadImageAlt,
-                file: {
-                    url: (() => {
-                        if (image.includes("https:")) {
-                            return image;
-                        }
-                        return `/img/from-cms/${image}`;
-                    })()
-                }
-            },
-            components: blogData.components,
-            source: "DecapCMS"
-        });
-    }
-
-
-    // Add preview blog (for CMS)
     blogs.push({
-        title: "Preview",
-        summaryLong: "[Optional summary goes here]",
-        authors: [{
-            name: "[Author goes here]",
-            jobTitle: "Job title and image (not available in preview)",
-            picture: {
-                fields: {
-                    file: {
-                        url: "https://t4.ftcdn.net/jpg/00/93/85/69/360_F_93856984_YszdhleLIiJzQG9L9pSGDCIvNu5GEWCc.jpg"
-                    }
-                }
+      title: blogData.title,
+      summaryShort: blogData.summaryHubPage,
+      summaryLong: blogData.summaryBlogPage,
+      authors: (() => {
+        let blogAuthors = blogData.authors?.map((author) => {
+          return author.author;
+        });
+        let foundAuthors = [];
+        allAuthors.forEach((author) => {
+          if (blogAuthors?.includes(author.name)) {
+            author.picture.fields.file.url = author.picture.fields.file.url.replace("/images/uploads/", "https://i-dot-ai-cms.netlify.app/assets/");
+            foundAuthors.push(author);
+          }
+        });
+        return foundAuthors;
+      })(),
+      date: blogData.date,
+      coverImage: {
+        title: blogData.leadImageCaption,
+        description: blogData.leadImageAlt,
+        file: {
+          url: (() => {
+            if (image.includes("https:")) {
+              return image;
             }
-        }],
-        date: new Date().toDateString(),
-        coverImage: blogs[0].coverImage,
-        components: [
-            {
-                type: "video"
-            }
-        ],
-        source: "DecapCMS"
+            return image.replace("/images/uploads/", "https://i-dot-ai-cms.netlify.app/assets/");
+          })(),
+        },
+      },
+      components: blogData.components,
+      source: "DecapCMS",
     });
+  }
 
+  // Add preview blog (for CMS)
+  blogs.push({
+    title: "Preview",
+    summaryLong: "[Optional summary goes here]",
+    authors: [
+      {
+        name: "[Author goes here]",
+        jobTitle: "Job title and image (not available in preview)",
+        picture: {
+          fields: {
+            file: {
+              url: "https://t4.ftcdn.net/jpg/00/93/85/69/360_F_93856984_YszdhleLIiJzQG9L9pSGDCIvNu5GEWCc.jpg",
+            },
+          },
+        },
+      },
+    ],
+    date: new Date().toDateString(),
+    coverImage: blogs[0].coverImage,
+    components: [
+      {
+        type: "video",
+      },
+    ],
+    source: "DecapCMS",
+  });
 
-    // sort by date (latest first)
-    blogs.sort((a, b) => {
-        return a.date < b.date ? 1 : -1;
-    });
-    return blogs;
-
+  // sort by date (latest first)
+  blogs.sort((a, b) => {
+    return a.date < b.date ? 1 : -1;
+  });
+  return blogs;
 };
