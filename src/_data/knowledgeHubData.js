3d3b0bc29c250eed065473c92a30713410da9421
Kevin Etchells <kevetchells@hotmail.com>
2025-05-23 13:14:01 +0100

    Add contact email to KH homepage

---
+++ src/_data/knowledgeHubData.js
@@ -0,0 +1,8 @@
+module.exports = async () => {
+
+  return {
+    // This also appears on the Knowledge Hub blog page, and the Guidance page content on the CMS
+    contactEmail: "ai-knowledge-hub@digital.cabinet-office.gov.uk"
+  };
+
+};
