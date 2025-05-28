ea4e61e159eca0bfeb4ece72fac3e132c6a2d034
Kevin Etchells <kevetchells@hotmail.com>
2025-05-28 13:38:02 +0100

    Update KH contact email

--- src/_data/knowledgeHubData.js
+++ src/_data/knowledgeHubData.js
@@ -2,7 +2,7 @@ module.exports = async () => {
 
   return {
     // This also appears on the Knowledge Hub blog page, and the Guidance page content on the CMS
-    contactEmail: "ai-knowledge-hub@digital.cabinet-office.gov.uk"
+    contactEmail: "ai-knowledge-hub@dsit.gov.uk"
   };
 
 };
