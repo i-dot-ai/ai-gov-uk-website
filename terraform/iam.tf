984066dd5866e4cbbaf18fbb51fd8d8e32c66206
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-07-29 08:36:21 +0100

    Remove s3:deleteobject in iam policy 1st statement

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,7 +3,6 @@ data "aws_iam_policy_document" "website" {
     effect = "Allow"
     actions = [
       "s3:GetObject",
-      "s3:DeleteObject",
     ]
     resources = [
       "${aws_s3_bucket.website.arn}/*",
