f55171a437d355db3d9afd931d0e28f7db9508e6
jvickers123 <jonathan.vickers@cabinetoffice.com>
2025-10-17 08:43:27 +0100

    update testing config: fix for deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Parallel tests on CI. */
-  workers: 1, // Running in parallel causing consistency issues
+  workers: process.env.CI ? 2 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
