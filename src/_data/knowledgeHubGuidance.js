525d80f57cd6e72958ad760ac42787fb64cc1371
jvickers123 <jonathan.vickers@cabinetoffice.com>
2025-11-13 09:35:51 +0000

    udpate guidance page initial

--- src/_data/knowledgeHubGuidance.js
+++ src/_data/knowledgeHubGuidance.js
@@ -1,26 +1,46 @@
 const getData = require("./_shared.js").getData;
 const yaml = require("yaml");
 
+const pageNameToDataNameMap = {
+  'guidance': 'guidance.yaml',
+  'plan-and-run-an-ai-project-a': 'guidance_dev_plan_run_v_a.yaml',
+  'plan-and-run-an-ai-project-b': 'guidance_dev_plan_run_v_b.yaml',
+  'using-ai-assistants-a': 'guidance_dev_v_a.yaml',
+  'using-ai-assistants-b': 'guidance_dev_v_b.yaml',
+  'playbook': 'playbook.yaml',
+  'principles': 'principles.yaml',
+  'procurement': 'procurement.yaml',
+  'legal': 'legal.yaml',
+  'ethics': 'ethics.yaml',
+  'ethics-building-ai': 'ethics_building_ai.yaml',
+  'understanding-ai': 'understanding_ai.yaml',
+  'building-ai-solutions': 'building_ai_solutions.yaml',
+  'security': 'security.yaml',
+  'governance': 'governance.yaml',
+  'measure-impact': 'measure_impact.yaml',
+}
+
 
 const CMS_REPO = "i-dot-ai/ai-gov-uk-cms-content";
 
-let cache;
+const cache = {};
 
 module.exports = async () => {
   
-  if (cache) {
+  if (Object.keys(cache).length > 0) {
     console.log('Using cached Knowledge Hub Guidance content');
     return cache;
   }
 
-  const guidanceData = await getData(
-    `https://api.github.com/repos/${CMS_REPO}/contents/content/knowledge_hub/guidance.yaml`
-  );
+  for (const [pageName, dataName] of Object.entries(pageNameToDataNameMap)) {
+    const guidanceData = await getData(
+      `https://api.github.com/repos/${CMS_REPO}/contents/content/knowledge_hub/${dataName}`
+    );
+    const yamlData = Buffer.from(guidanceData.content, "base64").toString("utf8");
+    const content = yaml.parse(yamlData).content;
+    cache[pageName] = content;
+  }
 
-  const yamlData = Buffer.from(guidanceData.content, "base64").toString("utf8");
-  const content = yaml.parse(yamlData).content;
-  
-  cache = content;
-  return content;
+  return cache;
 
 };
