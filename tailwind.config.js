27de04c5765e07f707174d963f952ed8ac35c86f
jvickers123 <jonathan.vickers@cabinetoffice.com>
2025-12-03 13:22:40 +0000

    Take build from astro repo: revert tailwind change 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["src/**/*.njk", "src/**/*.md"],
-    theme: {
-      container: {
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '3rem',
-          xl: '6rem',
-          '2xl': '6rem',
-        },
+  content: ["src/**/*.njk", "src/**/*.md"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '3rem',
+        xl: '6rem',
+        '2xl': '6rem',
       },
-      extend: {}
     },
-    plugins: [],
-    purge: false,
-  };
-  
+    extend: {},
+  },
+  plugins: [],
+  purge: false,
+};
